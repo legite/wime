11d6fe8fb98537c617b782b4cace77b317a21c9b
legite <vastlee@hotmail.com>
2017-06-19 12:28:07 +0800

    first hello

---
+++ src/main/listdemo0.java
@@ -0,0 +1,13 @@
+/**
+ * @author le by 6 - 19, 2017
+ */
+package main;
+
+public class listdemo0 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello list!");
+	}
+
+}
