6ffcb35d4d49b54bae11b6d62b60f46b689a1b84
legite <vastlee@hotmail.com>
2017-06-19 12:55:48 +0800

    listdemo

--- src/main/listdemo0.java
+++ src/main/listdemo0.java
@@ -3,11 +3,48 @@
  */
 package main;
 
+import java.util.*;
+
 public class listdemo0 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello list!");
+		List<String> a = new LinkedList<>();
+		a.add("Amy");
+		a.add("Carl");
+		a.add("Eric");
+		
+		List<String> b = new LinkedList<>();
+		b.add("Bob");
+		b.add("Doug");
+		b.add("Frances");
+		b.add("Gloria");
+		
+		ListIterator<String> aIter = a.listIterator();
+		Iterator<String> bIter = b.iterator();
+		
+		while(bIter.hasNext()){
+			if (aIter.hasNext()) {
+				aIter.hasNext();
+			}
+			aIter.add(bIter.next());
+		}
+		
+		System.out.println(a);
+		
+		bIter = b.iterator();
+		while(bIter.hasNext()){
+			bIter.next();
+			if(bIter.hasNext()){
+				bIter.next();
+				bIter.remove();
+			}
+		}
+		
+		System.out.println(b);
+		
+		System.err.println(a);
 	}
 
 }
