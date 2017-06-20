a4e03121123873d3188757a802d3936821b95eb1
legite <vastlee@hotmail.com>
2017-06-20 17:28:58 +0800

    list demo 2

---
+++ src/main/listdemo2.java
@@ -0,0 +1,13 @@
+/**
+ * @author le
+ */
+package main;
+
+public class listdemo2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello list");
+		System.out.println("i'm coming!");
+	}
+
+}
