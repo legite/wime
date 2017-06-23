20517a76d30ac3b8974a9ce75f79b43cc12c0bc2
legite <vastlee@hotmail.com>
2017-06-23 11:43:47 +0800

    k list merge

---
+++ src/main/Merge_K_list.java
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+
+package main;
+
+public class Merge_K_list {
+	public ListNode mergeKLists(ListNode[] lists) {
+		
+		
+		
+		return null;
+	}
+	
+}
